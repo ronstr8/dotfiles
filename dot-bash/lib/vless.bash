a7eb8e6076df935af1db1611c00636f7cc787fae
Ronald E. Straight <straight@adiant.com>
2015-08-17 10:45:02 -0400

    Libified vless (and some screen stuff) from .bash/aliases.

--- dot-bash/lib/vless.bash
+++ dot-bash/lib/vless.bash
@@ -1,15 +1,16 @@
 
 if ! pingLib ${BASH_SOURCE[0]} ; then
 
-unset -f vless ;
-function vless() {
-	local params="$@" ;
+needsLib 'proc' ;
 
-	if ! tty --silent ; then
-		params="${params} -" ;	
-	fi
+unset -f vless ; function vless() {
+	local -a params="$@" ;
 
-	eval "vim -R -c 'let no_plugin_maps = 1' -c 'runtime! macros/less.vim' $params" ;
+    if [[ $# -ne 1 || "$1" != '-' ]] && ( isPiped || isRedirected ) ; then
+		params[${#params[@]}]='-' ;
+    fi
+
+	eval "vim -n -R -c 'let no_plugin_maps = 1' -c 'runtime! macros/less.vim' ${params[@]}" ;
 }
 
 touchLib ${BASH_SOURCE[0]} ; fi ;
