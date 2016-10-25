e863ca7ec8cb638752debc4f057af24c82c5e892
Ronald E. Straight <straight@adiant.com>
2016-10-25 13:07:53 -0400

    Added `necho' command using bash /dev/tcp.

---
+++ dot-bash/lib/net.bash
@@ -0,0 +1,32 @@
+
+if ! pingLib ${BASH_SOURCE[0]} ; then
+
+## necho <host> <port> ...
+#
+#   Echo text to a TCP host/port.  `echo -ne' is used to emit the text.
+#
+# @see http://www.linuxjournal.com/content/more-using-bashs-built-devtcp-file-tcpip
+# @see http://www.tldp.org/LDP/abs/html/devref1.html
+##
+unset -f necho ; function necho() {
+    declare    host=${1?Missing host argument.} ; shift ;
+    declare -i port=${1?Missing port argument.} ; shift ;
+
+    declare    sock=$( printf '/dev/tcp/%s/%d' "$host" $port ) ;
+
+    if exec 13<>$sock ; then
+        echo -ne "$@" >&13 ;
+        cat <&13 ;
+    else
+        echo "Failed to create socket $sock!" >&2 ;
+    fi
+
+    exec 13>&- ;
+} ;
+
+touchLib ${BASH_SOURCE[0]} ; fi ;
+
+## vim: ft=sh
+
+
+
