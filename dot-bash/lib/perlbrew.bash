b5da9068b47a0913ba0d865c5e0ff56edd40ad90
Ronald E. Straight <straight@adiant.com>
2017-03-07 13:39:15 -0500

    Some rakudobrew futzing about in perlbrew.bash.

--- dot-bash/lib/perlbrew.bash
+++ dot-bash/lib/perlbrew.bash
@@ -1,7 +1,19 @@
 
 if ! pingLib ${BASH_SOURCE[0]} ; then
 
+PERLBREW_BASHRC="$HOME/perl5/perlbrew/etc/bashrc" ;
+
 [ -f "$HOME/perl5/perlbrew/etc/bashrc" ] && . "$HOME/perl5/perlbrew/etc/bashrc"
+
+RAKUDOBREW_BIN="$HOME/.rakudobrew/bin/rakudobrew" ;
+
+# git clone https://github.com/tadzik/rakudobrew ~/.rakudobrew
+# echo 'export PATH=~/.rakudobrew/bin:$PATH' >> ~/.bashrc
+# apt-get install build-essential git libssl-dev
+# eval "$(/home/ronstra/.rakudobrew/bin/rakudobrew init -)"
+
+# Broken right now.  :(
+# [ -x $RAKUDOBREW_BIN ] && eval $( $RAKUDOBREW_BIN init - ) ;
 
 touchLib ${BASH_SOURCE[0]} ; fi ;
 
