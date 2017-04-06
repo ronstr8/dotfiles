5dd7d876695622a428a4adedc79250890bfad7c1
Ronald E. Straight <straight@adiant.com>
2017-04-06 10:21:38 -0400

    Restored lib/screen functions, incl. session-checkpoint.

--- dot-bash/lib/screen.bash
+++ dot-bash/lib/screen.bash
@@ -10,25 +10,25 @@ function screen-stuff() {
     screen -X at \# stuff "$*\015" ;
 } ;
 
-## function session-checkpoint() {
-##     if [[ ! "$SESSION_HOME" ]] ; then
-##         echo "${FUNCNAME}: No \$SESSION_HOME defined.  Aborting." >&2 ;
-##         return $RV_FAILURE ;
-##     fi
-## 
-##     declare thisWinDir="${WINDOW_HOME:-}" eachWindir='' ;
-## 
-##     for eachWinDir in $SESSION_HOME/window-[0-9][0-9] ; do
-##         [[ $eachWinDir != $thisWinDir ]] && screen -X at ${eachWinDir/#*window-} stuff "eval \$( session --eval )\015" ;
-##     done
-## 
-##     eval $( session --eval ) ; 
-## 
-##     session --screenrc ;
-##     session --status   ;
-## 
-##     [[ ! -f "$HISTFILE" ]] || [[ "$HISTFILE" -nt "$WAITFILE" ]] ;
-## } ;
+function session-checkpoint() {
+    if [[ ! "$SESSION_HOME" ]] ; then
+        echo "${FUNCNAME}: No \$SESSION_HOME defined.  Aborting." >&2 ;
+        return $RV_FAILURE ;
+    fi
+
+    declare thisWinDir="${WINDOW_HOME:-}" eachWindir='' ;
+
+    for eachWinDir in $SESSION_HOME/window-[0-9][0-9] ; do
+        [[ $eachWinDir != $thisWinDir ]] && screen -X at ${eachWinDir/#*window-} stuff "eval \$( session --eval )\015" ;
+    done
+
+    eval $( session --eval ) ; 
+
+    session --screenrc ;
+    session --status   ;
+
+    [[ ! -f "$HISTFILE" ]] || [[ "$HISTFILE" -nt "$WAITFILE" ]] ;
+} ;
 
 touchLib ${BASH_SOURCE[0]} ; fi ;
 
